556c30f35e9a5ebeae7b0eb43c3432e8dc1bc5a8
Sergey Lapin <sergey.lapin@cogentembedded.com>
2019-02-10 06:13:04 +0300

    Fixed dialogue mode

--- characters/dialogue/DialogueScheduler.gd
+++ characters/dialogue/DialogueScheduler.gd
@@ -21,22 +21,137 @@ var dialogue_state = "thesis"
 var texts = {
 	"greeting": {
 		"thesis": [
-			["Hi!", ["neutral"], ["greeting"]],
-			["Hello!", ["neutral"], ["greeting"]],
-			["Hello bitches!", ["evil"], ["greeting", "insult"]]
+			["#greetings#", [], ["greeting"]],
 		],
 		"answer": [
-			["Hi!", ["neutral"], ["greeting"]],
-			["Hello!", ["neutral"], ["greeting"]],
-			["Greetings!", ["neutral"], ["greeting"]]
+			["#greetings#", [], ["greeting"], ["weather"]],
 		]
-	}
+	},
+	"weather": {
+		"thesis": [
+			["So #appreciation# weather today!", ["sane"], ["chatter"]],
+			["So fucking #appreciation# weather today!", ["sane", "evil"], ["chatter"]],
+			["Yaiiiieeeeekes!", ["broken"], ["chatter"]],
+			["Glugluglu!", ["broken"], ["chatter"]],
+		],
+		"answer": [
+			["Yeah, weather is #appreciation#.", ["neutral", "sane"], ["chatter"], ["weather", "endings"]],
+			["Well, if you say so.", ["neutral", "sane"], ["chatter"], ["weather", "endings"]],
+			["Yeah, weather is fucking #appreciation#.", ["evil", "sane"], ["chatter"], ["weather", "endings"]],
+			["Yuuuu... amm... #appreciation#...", ["broken"], ["chatter"], ["endings", "weather"]],
+			["Yuuuu... err... #appreciation#...", ["broken"], ["chatter"], ["endings", "weather"]],
+			["Eeeeek... whatever... #appreciation#...", ["broken"], ["chatter"], ["weather", "endings"]],
+		]
+	},
+	"endings": {
+		"thesis": [
+			["Bye guys!", ["neutral", "sane", "multiple_characters"], ["end"]],
+			["Farewell people...", ["neutral", "sane", "multiple_characters"], ["end"]],
+			["Farewell #plural_insult_nouns#!", ["neutral", "sane", "multiple_characters"], ["end"]],
+			["Bye #singular_insult_nouns#!", ["evil", "sane", "single_character"], ["end"]],
+			["Bye.", ["neutral", "sane"], ["end"]],
+			["Tata.", ["broken"], ["end"]]
+		],
+		"answer": [
+			["Bye.", ["sane"], ["chatter"], ["weather"]],
+			["Tata.", ["broken"], ["chatter"], ["weather"]]
+		]
+	},
+	"greetings": [
+		["Hi!", ["neutral", "sane"], ["greeting"]],
+		["Hello!", ["neutral", "sane"], ["greeting"]],
+		["Hello #plural_insult_nouns#!", ["evil", "multiple_characters", "sane"], ["greeting", "insult"]],
+		["Hello #singular_insult_nouns#!", ["evil", "single_character", "sane"], ["greeting", "insult"]],
+		["...", ["broken"], ["greeting", "stupid"]],
+		["Aaaaahhhh", ["broken"], ["greeting", "stupid"]],
+		["Grrrrr", ["broken", "evil"], ["greeting", "stupid"]]
+	],
+	"plural_insult_nouns": [
+		["bitches", ["neutral", "sane"], ["insult"]],
+		["morons", ["neutral", "sane"], ["insult"]],
+		["imbiciles", ["neutral", "sane"], ["insult"]],
+		["degenerates", ["neutral", "sane"], ["insult"]],
+		["alcoholics", ["neutral", "sane"], ["insult"]],
+		["junkies", ["neutral", "sane"], ["insult"]],
+		["drunkies", ["neutral", "sane"], ["insult"]]
+	],
+	"singular_insult_nouns": [
+		["bitch", ["neutral", "sane"], ["insult"]],
+		["moron", ["neutral", "sane"], ["insult"]],
+		["imbicile", ["neutral", "sane"], ["insult"]],
+		["degenerate", ["neutral", "sane"], ["insult"]],
+		["alcoholic", ["neutral", "sane"], ["insult"]],
+		["junkie", ["neutral", "sane"], ["insult"]],
+		["drunkie", ["neutral", "sane"], ["insult"]]
+	],
+	"appreciation": [
+		["nice", [], ["chatter"]],
+		["good", [], ["chatter"]],
+		["beautiful", [], ["chatter"]],
+		["excellent", [], ["chatter"]]
+	]
 }
+func parse_rule(obj: Object, grammar:Dictionary, text: String, keywords: Array) -> Array:
+	var kw = keywords.duplicate()
+	print("kw: ", kw)
+	while text.find("#") >= 0:
+		var tstart = text.find("#") + 1
+		var tend = text.find("#", tstart + 1)
+		var l = tend - tstart
+		var token = text.substr(tstart, l)
+		print("subs: ", text.substr(tstart, l))
+		var item_list = filter_list(obj, grammar[token])
+		var item = item_list[randi() % item_list.size()]
+		text = text.replace("#" + token + "#", item[0])
+		for k in item[2]:
+			if !k in kw:
+				kw.push_back(k)
+	print("kw2: ", kw)
+	return [text, kw]
+	
+
+func match_keyword(obj, kw):
+	match(kw):
+		"neutral":
+			if !awareness.has_trait(obj, "evil"):
+				return true
+		"evil":
+			if awareness.has_trait(obj, "evil"):
+				return true
+		"broken":
+			if awareness.has_trait(obj, "broken"):
+				return true
+		"sane":
+			if !awareness.has_trait(obj, "broken"):
+				return true
+		"multiple_characters":
+			if character_list.size() > 2:
+				return true
+		"signle_character":
+			if character_list.size() == 2:
+				return true
+	return false
+func filter_list(obj, list):
+	var ret = []
+	for k in list:
+		var keywords = k[1]
+		var kw_matching = true
+		for kw in keywords:
+			if !match_keyword(obj, kw):
+				kw_matching = false
+		if kw_matching:
+			ret.push_back(k)
+	return ret
+			
 
 func generate_text(obj, state, dstate):
 	var list = texts[state][dstate]
-	var r = randi() % list.size()
-	return list[r][0]
+	list = filter_list(obj, list)
+	if list.size() > 0:
+		var r = randi() % list.size()
+		return list[r]
+	else:
+		return ["...", [],[],[]]
 
 func say(obj, text):
 	speech_text[obj].text = text
@@ -44,6 +159,7 @@ func say(obj, text):
 		speech_text[obj].show()
 	speech_text[obj].update()
 	print("say: ", text)
+var reactions = []
 func _process(delta):
 	if cooldown > 0.0:
 		cooldown -= delta
@@ -52,16 +168,30 @@ func _process(delta):
 			match(dialogue_state):
 				"thesis":
 					var obj = character_list[token]
-					say(obj, generate_text(obj, state, dialogue_state))
+					var item = generate_text(obj, state, dialogue_state)
+					var res = parse_rule(obj, texts, item[0], item[2])
+					say(obj, res[0])
+					reactions = res[1]
 					dialogue_state = "answer"
 					cooldown = dialogue_delay
 				"answer":
+					var last_state = state
 					for k in range(character_list.size()):
 						if k == token:
 							continue
+						print("dialogue_state: ", state, " ", dialogue_state)
 						var obj = character_list[k]
-						say(obj, generate_text(obj, state, dialogue_state))
-						dialogue_state = "thesis"
+						var item = generate_text(obj, state, dialogue_state)
+						var res = parse_rule(obj, texts, item[0], item[2])
+						say(obj, res[0])
+						print("dialogue_state: ", state, " ", dialogue_state)
+						last_state = item[3][randi() % item[3].size()]
+					print("reactions: ", reactions)
+					if "end" in reactions:
+						awareness.needs[character_list[token]]["Socialization"] = 0.0
+						remove_character(character_list[token])
+					dialogue_state = "thesis"
+					state = last_state
 					cooldown = dialogue_delay
 					token = randi() % character_list.size()
 	for h in character_list:
@@ -69,6 +199,12 @@ func _process(delta):
 		var pos = get_viewport().get_camera().unproject_position(box_position)
 		speech_text[h].rect_position = pos - Vector2(0, 32)
 		speech_text[h].rect_size = Vector2(120, 64)
+	if character_list.size() < 2:
+		print("closing dialogue")
+		remove_character(character_list[0])
+		character_list.clear()
+		set_process(false)
+		queue_free()
 
 func add_character(obj):
 	if !obj in character_list:
@@ -77,3 +213,16 @@ func add_character(obj):
 		get_node("/root").add_child(speech)
 		speech.hide()
 		speech_text[obj] = speech
+		state = "greeting"
+		print("added character ", obj)
+
+func remove_character(obj):
+	if obj in character_list:
+		var speech = speech_text[obj]
+		speech_text.erase(obj)
+		speech.queue_free()
+		if character_list[token] == obj:
+			token = 0
+		character_list.erase(obj)
+		awareness.dialogue_mode.erase(obj)
+		print("removed character ", obj)
